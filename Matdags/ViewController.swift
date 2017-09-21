b6cf5e6ec200a161813b7bcf629bb8c1c32b1838
NickeGilbert <nicklasgilbertson@Nicklass-MacBook-Pro.local>
2017-09-21 12:40:00 +0200

    First commit
    Test

--- Matdags/ViewController.swift
+++ Matdags/ViewController.swift
@@ -12,14 +12,44 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if Auth.auth().currentUser?.uid != nil {
+            goHome()
+        }
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    func login(){
+        Auth.auth().signIn(withEmail: emailText.text!, password: password.text!, completion: {
+            user, error in
+            
+            if error != nil{
+                
+                print ("Incorrect")
+            }
+            else{
+                self.goHome()
+                print("Correct")
+            }
+        })
+    }
+    
+    func goHome() {
+        let bildflodeVC = bildflodeViewController()
+        self.present(bildflodeVC, animated: true,
+                     completion: nil)
+    }
+    
+    @IBAction func logginButton(_ sender: Any) {
+        login()
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
