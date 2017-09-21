0562cffae0087ac1b3b983daafd6420644f693ed
Kevin Henriksson <henriksson.kevin@gmail.com>
2017-09-21 12:48:42 +0200

    Kamera stuff
    Basfiler och plistinfo för kamera inegration

---
+++ Matdags/CustomCamera.swift
@@ -0,0 +1,11 @@
+//  CustomCamera.swift
+//  Matdags
+//  Created by Kevin Henriksson on 2017-09-21.
+//  Copyright © 2017 Matdags. All rights reserved.
+
+import UIKit
+
+extension ViewController {
+    
+    //Placeholder för kamera kod
+}
