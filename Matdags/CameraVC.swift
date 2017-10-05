556974428e91bfb7a4e459dc0f52868caa570458
Kevin Henriksson <henriksson.kevin@gmail.com>
2017-10-05 11:26:25 +0200

    Warning cleanup, påbörjat kameraarbete
    Fixade en del varningar samt påbörjat arbete för att få upp kamera vid tryck på tabknapp

--- Matdags/CameraVC.swift
+++ Matdags/CameraVC.swift
@@ -8,6 +8,18 @@
 
 import UIKit
 
-class CameraVC: UIViewController {
+class CameraVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            var imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = .camera;
+            imagePicker.allowsEditing = false
+            self.presentViewController(imagePicker, animated: true, completion: nil)
+        }
+    }
 }
