660d8a450ea61de536974863566be117a209d9a2
Kevin Henriksson <henriksson.kevin@gmail.com>
2017-10-05 11:50:38 +0200

    Swift file för Tab Bar Controller

---
+++ Matdags/TabBarC.swift
@@ -0,0 +1,17 @@
+//
+//  TabBarC.swift
+//  Matdags
+//
+//  Created by Kevin Henriksson on 2017-10-05.
+//  Copyright © 2017 Matdags. All rights reserved.
+//
+
+import UIKit
+
+class TabBarC : UITabBarController {
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+    }
+}
