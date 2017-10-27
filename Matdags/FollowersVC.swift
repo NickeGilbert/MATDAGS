ca6702cdd8d0e25d6afa88b4a0a1e2e4ed494f2f
Nicklas Gilbertsson <nicklas.gilbertson@gmail.com>
2017-10-27 19:26:40 +0200

    extrakod
    extrakod

--- Matdags/FollowersVC.swift
+++ Matdags/FollowersVC.swift
@@ -14,14 +14,50 @@ class FollowersVC: UIViewController, UICollectionViewDelegate, UICollectionViewD
     
     @IBOutlet var feedCollectionView: UICollectionView!
     
+    var ref: DatabaseReference!
     var feedArray = [Images]()
     let cellIdentifier = "cell"
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+      /*  ref = Database.database().reference()
+        if let uid = Auth.auth().currentUser?.uid{
+            //To get the users we are following
+            ref.child("Followers").child(uid).observe(.value, with: { (followingSnapshot) in
+                //Store values in dictionary
+                let followingDictionary = followingSnapshot.value as! NSDictionary
+                for (id, _) in followingDictionary {
+                    //To get the users who are following images
+                    let userPostRef = self.ref.child("Post").child(id as! String) //Kolla ifall Matdags stämmer
+                    userPostRef.observe(.value, with: { (postSnapshots) in
+                        //Store values in a dictionary
+                        let postDictionary = postSnapshots.value as! NSDictionary
+                        for(p) in postDictionary {
+                            let posts = p.value as! NSDictionary
+                            //get profile photo url => profile_pic
+                            guard let profileUrl = posts.value(forKey: "profile_pic") else{return}
+                            //get username Key => username
+                            guard let username = posts.value(forKey: "username") else{return} //posts ska kanske vara Images
+                            //get post url Key => posted_pic
+                            guard let postUrl = posts.value(forKey: "posted_pic") else{return} //posts ska kanske vara Images
+                            
+                            let newPost: Images = Images(profilePic: profileUrl as! String, username: username as! String, postImage: postUrl as! String)
+                            self.feedArray.append(newPost)
+                        }
+                    })
+                }
+            })
+        }*/
+    }
+        
     
     func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 1
     }
-   
-   func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int{
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int{
         return feedArray.count
     }
     
@@ -30,9 +66,4 @@ class FollowersVC: UIViewController, UICollectionViewDelegate, UICollectionViewD
         
         return cell
     }
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        }
 }
