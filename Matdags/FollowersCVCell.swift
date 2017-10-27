7970dbf94b587e7df54c0cc6ee587abaa621b7c5
Nicklas Gilbertsson <nicklas.gilbertson@gmail.com>
2017-10-27 14:10:03 +0200

    smågrejer
    smågrejer

--- Matdags/FollowersCVCell.swift
+++ Matdags/FollowersCVCell.swift
@@ -9,7 +9,27 @@
 import UIKit
 
 class FollowersCVCell: UICollectionViewCell {
-
+    
+    @IBOutlet var profileImage: UIImageView!
+    @IBOutlet var username: UILabel!
+    @IBOutlet var postImageView: UIImageView!
+    
+    @IBOutlet var starButtons: [UIButton]!
+    
+    
+    @IBAction func starButtonsTapped(_ sender: UIButton) {
+        
+        let tag = sender.tag
+        for button in starButtons {
+            if button.tag <= tag {
+                button.setTitle("⭐️", for: .normal)
+            } else {
+                button.setTitle("☆", for: .normal)
+            }
+        }
+    }
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
