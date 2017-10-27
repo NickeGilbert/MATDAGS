0c48dc8ac97eb5d819cca740da8920b4966868ae
Nicklas Gilbertsson <nicklas.gilbertson@gmail.com>
2017-10-27 13:52:55 +0200

    Revert "extra saker"
    This reverts commit 4d23d9dee81aa305bac6e6400e3c611ba5210694.

--- Matdags/FollowersCVCell.swift
+++ Matdags/FollowersCVCell.swift
@@ -9,25 +9,7 @@
 import UIKit
 
 class FollowersCVCell: UICollectionViewCell {
-    
-    @IBOutlet var profileImage: UIImageView!
-    @IBOutlet var username: UILabel!
-    @IBOutlet var imageFeedView: UIImageView!
-    
-    @IBOutlet var starButton: [UIButton]!
-    
-    @IBAction func starButtonTapped(_ sender: UIButton) {
-        let tag = sender.tag
-        for button in starButton {
-            if button.tag <= tag {
-                button.setTitle("⭐️", for: .normal)
-            } else {
-                button.setTitle("☆", for: .normal)
-            }
-        }
-    }
-    
-    
+
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
