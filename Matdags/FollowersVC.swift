783f5f6f9f7088f37c9c9438f9b56f00c9a0a786
Nicklas Gilbertsson <nicklas.gilbertson@gmail.com>
2017-10-28 13:34:53 +0200

    Gjort om kod för följare
    Gjort om kod för följare

--- Matdags/FollowersVC.swift
+++ Matdags/FollowersVC.swift
@@ -18,40 +18,47 @@ class FollowersVC: UIViewController, UICollectionViewDelegate, UICollectionViewD
     var feedArray = [Images]()
     let cellIdentifier = "cell"
     
+    var count:Int! = 0
+    var lastCount:Int! = 0
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-      /*  ref = Database.database().reference()
+        /*ref = Database.database().reference()
         if let uid = Auth.auth().currentUser?.uid{
             //To get the users we are following
-            ref.child("Followers").child(uid).observe(.value, with: { (followingSnapshot) in
+            ref.child("Followers").child(uid).observe(.value, with: { (followersSnapshot) in
                 //Store values in dictionary
-                let followingDictionary = followingSnapshot.value as! NSDictionary
-                for (id, _) in followingDictionary {
+                let followersDictionary = followersSnapshot.value as! NSDictionary
+                for (id, _) in followersDictionary {
                     //To get the users who are following images
-                    let userPostRef = self.ref.child("Post").child(id as! String) //Kolla ifall Matdags stämmer
-                    userPostRef.observe(.value, with: { (postSnapshots) in
+                    let userPostRef = self.ref.child("Post").child(id as! String)
+                    userPostRef.observe(.childAdded, with: { (postSnapshot) in
                         //Store values in a dictionary
-                        let postDictionary = postSnapshots.value as! NSDictionary
-                        for(p) in postDictionary {
-                            let posts = p.value as! NSDictionary
+                        if let userPostDictionary = postSnapshot.value as? NSDictionary{
+                            let postId = postSnapshot.key
+                            if let postData = postSnapshot.value as? NSDictionary{
                             //get profile photo url => profile_pic
-                            guard let profileUrl = posts.value(forKey: "profile_pic") else{return}
+                            guard let profileURL = postData["profile_pic"] as! String! else{return}
                             //get username Key => username
-                            guard let username = posts.value(forKey: "username") else{return} //posts ska kanske vara Images
+                            guard let username = postData["username"] as! String! else{return} //posts ska kanske vara Images
                             //get post url Key => posted_pic
-                            guard let postUrl = posts.value(forKey: "posted_pic") else{return} //posts ska kanske vara Images
+                            guard let postURL = postData["posted_pic"] as! String! else{return} //posts ska kanske vara Images
                             
-                            let newPost: Images = Images(profilePic: profileUrl as! String, username: username as! String, postImage: postUrl as! String)
-                            self.feedArray.append(newPost)
+                            self.feedArray.append(Images(postID: postId, profilePic: profileURL, username: username, postImage: postURL, timestamp: NSNumber(value: Int(NSDate().timeIntervalSince1970))))
+                            self.feedCollectionView.reloadData()
+                            }
                         }
+                    }, withCancel: { (error1) in
+                        print(error1)
                     })
                 }
+            }, withCancel: { (error2) in
+                print(error2)
             })
         }*/
     }
-        
     
     func numberOfSections(in collectionView: UICollectionView) -> Int {
         return 1
--- Matdags/Images.swift
+++ Matdags/Images.swift
@@ -8,14 +8,23 @@ import Foundation
 class Images {
     
     var fbKey = ""
+    var postID: String
     var username: String
     var profilePic: String
     var postImage: String
+    var timestamp: NSNumber
     
-    init(profilePic: String, username: String, postImage: String) {
+    init( postID: String, profilePic: String, username: String, postImage: String, timestamp:NSNumber) {
+        self.postID = postID
         self.profilePic = profilePic
         self.username = username
         self.postImage = postImage
+        self.timestamp = timestamp
+    }
+    
+    func returnPostAsDictionary()->NSDictionary{
+        let postDictionary: NSDictionary = ["profile_pic": profilePic, "username": username, "posted_pic": postImage]
+        return postDictionary
     }
     
 }
