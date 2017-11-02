f38bf7c2e832b566e3e18328e239b97bd5673378
Kevin Henriksson <henriksson.kevin@gmail.com>
2017-11-02 15:12:32 +0100

    Firebase Storage + Databas för uppladning
    Bilder kan nu laddas upp till firebase storage via uppladdningssidan. (enbart med tagen kamerabild för tillfället)
    
    Det skapas även en struktur i databasen för hämtning av post info osv osv.

--- Matdags/ImagePreVC.swift
+++ Matdags/ImagePreVC.swift
@@ -5,14 +5,19 @@
 
 import UIKit
 import AVFoundation
+import Firebase
 
-class ImagePreVC: UIViewController{
+class ImagePreVC: UIViewController {
+    
+    var s_image = [Images]()
     
     @IBOutlet weak var photo: UIImageView!
     var image: UIImage!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        print("\(String(describing: Auth.auth().currentUser?.uid))")
         
         photo.image = self.image
     }
@@ -23,7 +28,49 @@ class ImagePreVC: UIViewController{
     
     @IBAction func saveButton(_ sender: Any) {
         UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
+        UploadImageToFirebase()
         dismiss(animated: false, completion: nil)
     }
     
+    func UploadImageToFirebase() {
+        
+        let uid = Auth.auth().currentUser?.uid
+        let database = Database.database().reference()
+        let storage = Storage.storage().reference()
+        
+        let key = database.child("Posts").childByAutoId().key
+        let imageRef = storage.child("images").child(uid!).child("\(key).jpg")
+        
+        if let imageData = UIImageJPEGRepresentation(image!, 0.6){
+            let uploadMetadata = StorageMetadata()
+            uploadMetadata.contentType = "image/jpeg"
+            
+            let uploadTask = imageRef.putData(imageData, metadata: uploadMetadata) { (metadata, error) in
+                if error != nil{
+                    print("Det blev lite fel \(String(describing: error?.localizedDescription))")
+                    return
+                }
+                    print("Det gick bra.. den här gången. \(String(describing: metadata))")
+                    print("URL till bilden är \(String(describing: metadata?.downloadURL()))")
+                
+                    imageRef.downloadURL(completion: { (url, error) in
+                    if let url = url {
+                        let feed = ["userID" : uid,
+                                    "pathToImage" : url.absoluteString,
+                                    "likes" : 0,
+                                    "author" : Auth.auth().currentUser?.displayName,
+                                    "postID" : key] as [String: Any]
+                        
+                        let postFeed = ["\(key)" : feed]
+                        database.child("Posts").updateChildValues(postFeed)
+                    }
+                })
+            }
+            
+            uploadTask.resume()
+            
+        } else {
+            print("Bilden kunde inte konverteras!")
+        }
+    }
 }
