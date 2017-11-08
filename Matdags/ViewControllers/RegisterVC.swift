b6fa5c9e92a9c24ed23fa213e5ecdb1b1e3934d0
Daniel Barwén Trollvik <31731785+agoofih@users.noreply.github.com>
2017-11-08 21:12:00 +0100

    fixed info alert

--- Matdags/ViewControllers/RegisterVC.swift
+++ Matdags/ViewControllers/RegisterVC.swift
@@ -104,4 +104,12 @@ class RegisterVC: UIViewController, UITextFieldDelegate {
         //        }))
         self.present(alert, animated: true, completion: nil)
     }
+    
+    
+    @IBAction func infoClick(_ sender: Any) {
+        
+        createAlertRegister(title: "Användardata", message: "Informationen du ger ifrån dig genom att skapa ett konto med din mailadress, alias och lösenord varken delas till andra eller används av oss själva förutom för att möjliggöra inloggning med historik på flera enheter. ")
+        
+    }
+    
 }
